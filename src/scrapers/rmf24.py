d9933de2150dfb299ba1687c5944fcfcfbb1fdcc
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-05-09 14:45:08 +0200

    add rmf24 chart parsing

--- src/scrapers/rmf24.py
+++ src/scrapers/rmf24.py
@@ -31,3 +31,39 @@ def remove_suffix(string, suffix):
     else:
         return string
 
+
+def scrape_page_data(url=url):
+    """
+    Scrapes data from rmf's chart
+    :param url url of the website with data
+    """
+    # Load web page
+    page = request.urlopen(url)
+    # Create parser
+    soup = bs(page.read(), "html.parser")
+    # Data is stored in the last script
+    data = str(soup.body.find('script'))
+
+    # find lists of data
+    (sick, deaths, recovers) = re.findall(r"\[\[.*\]\]", data)
+
+    sick = sick.split("],[")
+    deaths = deaths.split("],[")
+    recovers = recovers.split("],[")
+
+    sick = [remove_suffix(remove_prefix(remove_prefix(
+        i, "[["), "Date.UTC"), "]]") for i in sick]
+    deaths = [remove_suffix(remove_prefix(remove_prefix(
+        i, "[["), "Date.UTC"), "]]") for i in deaths]
+    recovers = [remove_suffix(remove_prefix(remove_prefix(
+        i, "[["), "Date.UTC"), "]]") for i in recovers]
+
+    sick = [i.split("),") for i in sick]
+    deaths = [i.split("),") for i in deaths]
+    recovers = [i.split("),") for i in recovers]
+
+    sick = [["-".join(i[0][1:].split(",")), i[1]] for i in sick]
+    deaths = [["-".join(i[0][1:].split(",")), i[1]] for i in deaths]
+    recovers = [["-".join(i[0][1:].split(",")), i[1]] for i in recovers]
+
+    return {"sick": sick, "deaths": deaths, "recovers": recovers}
