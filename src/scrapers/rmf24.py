00ff8fee67a68f1e54ada5c2ad17f00846ba5837
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-05-09 16:12:35 +0200

    move utilities functions to separate package

--- src/scrapers/rmf24.py
+++ src/scrapers/rmf24.py
@@ -5,30 +5,9 @@ import pandas as pd
 from bs4 import BeautifulSoup as bs
 
 url = "https://www.rmf.fm/inc/outer/korona-wykres/wykres.html"
+from utils.affixes import remove_prefix
+from utils.affixes import remove_suffix
 
-
-def remove_prefix(string, prefix):
-    """
-    Removes unwanted prefix from string
-    :param string string to edit
-    :param prefix patter for prefix
-    """
-    if prefix in string[:len(prefix)]:
-        return string[len(prefix):]
-    else:
-        return string
-
-
-def remove_suffix(string, suffix):
-    """
-    Removes unwanted suffix from string
-    :param string string to edit
-    :param suffix patter for suffix
-    """
-    if suffix in string[:len(suffix)]:
-        return string[len(suffix):]
-    else:
-        return string
 
 
 def scrape_page_data(url=url):
