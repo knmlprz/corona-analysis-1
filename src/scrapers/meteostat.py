82c0eea4001c7146d41d1a988368ceac58f48712
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-05-10 12:29:58 +0200

    add meteostat data

---
+++ src/scrapers/meteostat.py
@@ -0,0 +1,32 @@
+from urllib import request
+from json import JSONDecoder
+from os import environ
+from sys import stderr
+
+import pandas as pd
+
+URL = "https://api.meteostat.net/v1/history/daily?station={}&start={}&end={}&key={}"
+STATION = "12375"  # Warsaw
+
+
+def get_data(end_date: str,
+             key: str = None,
+             start_date: str = "2020-02-01",
+             url: str = URL,
+             station: str = STATION,
+             ) -> dict:
+    if key is None:
+        try:
+            key = environ["meteostat"]
+        except KeyError:
+            print("There's no key", file=stderr)
+    decoder = JSONDecoder()
+    binary_data = decoder.decode(s=str(request.urlopen(
+        url.format(station, start_date, end_date, key)).read()))
+    data = decoder.decode(binary_data.decode("utf8"))["data"]
+
+    return data
+
+
+def convert(data: dict) -> pd.DataFrame:
+    return pd.DataFrame(data)
