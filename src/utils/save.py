61673c45a04d25cdfe6aa0c7b0c37d06fbc25533
Patryk Gronkiewicz <gronek.konta@gmail.com>
2020-05-14 21:19:31 +0200

    fix paths to store data in system cache directory

--- src/utils/save.py
+++ src/utils/save.py
@@ -1,10 +1,12 @@
 import pandas as pd
+from os import environ
+from sys import platform
 
 
 def save_dataframe(
         data: pd.DataFrame,
         filename: str,
-        destination: str = "./data"):
+        destination: str = None):
     """
     Saves given dataframe to CSV file
 
@@ -14,7 +16,15 @@ def save_dataframe(
         given data in pandas format
     filename : str
         filename without extension
-    destination : str
-        data folder destination (defaults to "./data")
+    destination : str, optional
+        data folder destination (defaults to system cache directory)
     """
+    if destination is None:
+        if platform == "linux":
+            destination = environ["HOME"] + "/.cache/ca1/"
+        if platform == "win32":
+            destination = environ["localappdata"] + "\\ca1\\"
+        if platform == "darwin":
+            destination = environ["HOME"] + "/Library/Caches/ca1/"
+
     data.to_csv(destination + filename + ".csv", index=False, mode="w+")
