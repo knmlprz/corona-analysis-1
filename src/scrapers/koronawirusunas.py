52e7161669f7c945386a7c9c2e07eacfd24824cd
finloop <piotr.krawiec23@gmail.com>
2020-05-14 15:25:35 +0200

    change output dir

--- src/scrapers/koronawirusunas.py
+++ src/scrapers/koronawirusunas.py
@@ -5,9 +5,11 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 from sys import stderr
+from src.utils.paths import get_path
 
 URL = "https://www.koronawirusunas.pl"
-PATTERN = re.compile("var\sdataSource_mobilnosc")
+PATTERN = re.compile(r"var\sdataSource_mobilnosc")
+
 
 def scrape_page_data():
     """
@@ -17,17 +19,17 @@ def scrape_page_data():
     # Load webpage
     web = urllib.request.urlopen(URL)
     soup = BeautifulSoup(web.read(), "lxml")
+    outputdir = get_path(subdir="koronawirusunas")
 
     # Get first scrpit tag that contains PATTERN
     script = soup.find('script', text=PATTERN)
+
     # Get all strings like 'var * = *;'
     js_vars_data = re.findall(
         r'var.*?=\s*(.*?);', script.string, re.DOTALL | re.MULTILINE)
 
     js_vars_names = re.findall(r"var\s*(\S*)", script.string)
-    print(js_vars_names)
 
-    print(script)
     # Parse each var to json array
     for i in range(0, len(js_vars_data)):
         # Substitute key: val to "key" val
@@ -43,21 +45,17 @@ def scrape_page_data():
             # Load it directly to dataframe
             df = pd.DataFrame(o)
 
-            # Export df to "./Data/koronawirusunas/file_name.csv"
+            # Export df to "data/koronawirusunas/file_name.csv"
             # Create directory if not exists
-            outdir = "./data/koronawirusunas"
-            outname = js_vars_names[i] + ".csv"
-            if not os.path.exists(outdir):
-                os.makedirs(outdir)
+            filename = js_vars_names[i] + ".csv"
 
-            df.to_csv(os.path.join(outdir, outname), sep="\t")
+            df.to_csv(os.path.join(outputdir, filename), sep="\t")
 
             print("\033[92mFrame found: {:.20} \033[0m".format(
                 js_vars_names[i]))
             print(df)
 
         except json.decoder.JSONDecodeError:
-            # Decoder Exception.
             print("Data skipped: {:.20}".format(r), file=stderr)
 
 
