4bd0f3318d82f75b0266c06ce74abedf5d686487
Piotr Krawiec <piotr.krawiec23@gmail.com>
2020-05-15 15:09:17 +0200

    refactor koronawirusunas

--- src/scrapers/koronawirusunas.py
+++ src/scrapers/koronawirusunas.py
@@ -1,11 +1,9 @@
-import json
 import re
 import urllib.request
 from bs4 import BeautifulSoup
 import pandas as pd
-import os
-from sys import stderr
 from src.utils.paths import get_path
+import demjson
 
 URL = "https://www.koronawirusunas.pl"
 PATTERN = re.compile(r"var\sdataSource_mobilnosc")
@@ -16,47 +14,18 @@ def scrape_page_data():
     Scrapes dataset from koronawirus.pl
 
     """
-    # Load webpage
     web = urllib.request.urlopen(URL)
     soup = BeautifulSoup(web.read(), "lxml")
     outputdir = get_path(subdir="koronawirusunas")
 
-    # Get first scrpit tag that contains PATTERN
+    # Get first script tag that contains PATTERN
     script = soup.find('script', text=PATTERN)
 
-    # Get all strings like 'var * = *;'
-    js_vars_data = re.findall(
-        r'var.*?=\s*(.*?);', script.string, re.DOTALL | re.MULTILINE)
+    # Group all vars to ('var name,'[data]')
+    vars = re.findall(
+        r'var\s*(.*?)\s*=(\s*\[[\s\S]*?\]);', script.string)
 
-    js_vars_names = re.findall(r"var\s*(\S*)", script.string)
-
-    # Parse each var to json array
-    for i in range(0, len(js_vars_data)):
-        # Substitute key: val to "key" val
-        r = re.sub(r'([{\s,])(\w+)(:)', r'\1"\2"\3', js_vars_data[i])
-
-        # Fix ',' before } ]
-        r = re.sub(r",[ \t\r\n]*}", "}", r)
-        r = re.sub(r",[ \t\r\n]*\]", "]", r)
-
-        try:
-            o = json.loads(r)
-            # Each array looks like [{"key": val, ...}, {"key: val}...]
-            # Load it directly to dataframe
-            df = pd.DataFrame(o)
-
-            # Export df to "data/koronawirusunas/file_name.csv"
-            # Create directory if not exists
-            filename = js_vars_names[i] + ".csv"
-
-            df.to_csv(os.path.join(outputdir, filename), sep="\t")
-
-            print("\033[92mFrame found: {:.20} \033[0m".format(
-                js_vars_names[i]))
-            print(df)
-
-        except json.decoder.JSONDecodeError:
-            print("Data skipped: {:.20}".format(r), file=stderr)
+    return {t[0]: pd.DataFrame(demjson.decode(t[1])) for t in vars}
 
 
 if __name__ == '__main__':
