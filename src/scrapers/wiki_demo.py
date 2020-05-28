71ec8ae39e4cf77603e8aaf3e2b8b6cbea478fc9
finloop <>
2020-05-28 09:25:30 +0200

    scrape wiki demo
    Added wiki_demo/get_data that
    scrapes data from wiki demographic page,
    returns dict with df.

---
+++ src/scrapers/wiki_demo.py
@@ -0,0 +1,36 @@
+"""
+Gathers demographic data from
+https://pl.wikipedia.org/wiki/Podzia%C5%82_administracyjny_Polski
+"""
+
+import pandas as pd
+
+URL = r"https://pl.wikipedia.org/wiki/Podzia%C5%82_administracyjny_Polski"
+
+
+def get_data(url=URL):
+    """
+        Scrapes data from wiki table
+
+        ...
+
+        Attributes
+        ----------
+        url : str
+            url of the website with data
+        """
+
+    # Load data from web
+    df = pd.read_html(url)[0]
+
+    # Drop unused cols
+    df.drop(axis=1, labels=["Herb", "Flaga"], inplace=True)
+
+    # Dorop empty rows
+    df.dropna(axis=0, inplace=True)
+
+    return {"demografia": df}
+
+
+if __name__ == '__main__':
+    print(get_data())
